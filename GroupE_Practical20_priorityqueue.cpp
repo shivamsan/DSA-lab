4ae077d61866f485d6d08604413ca4e099724b9c
Shivam Singh <100526291+shivamsan@users.noreply.github.com>
2023-04-28 15:13:21 +0530

    Add files via upload

---
+++ GroupE_Practical20_priorityqueue.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout <<"enter the number of patients : " <<endl;
+    cin>>n;
+    int a[n],p[n];
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<"enter serial number of patient: "<<endl;
+        cin>>a[i];
+        cout <<"enter the priority of patient "<<a[i]<<" : " <<endl;
+        cin>>p[i];
+
+    }
+
+
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        if(p[j]>p[j+1])
+        {
+            int temp1 = p[j];
+                p[j] = p[j+1];
+                p[j+1] = temp1;
+            
+            int temp2 = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp2;
+            
+
+        }
+    }
+
+    cout<<"The patients on priority  basis are: "<<endl;
+    cout<<endl;
+    cout<<"serial number \t"<<"priority \t"<< "ward "<<endl;
+    cout<<endl;
+
+    for(int i=0;i<n;i++)
+    {
+
+        cout<<a[i]<<" \t\t";
+        cout<<p[i]<<" \t\t";
+
+        if(p[i]==1)
+        {
+            cout<<"icu"<<" ";
+        }
+        else if(p[i]==2)
+        {
+            cout<<"special"<<" ";
+        }
+        else
+        {
+            cout<<"general"<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
+
+}
